94d428d8fc9f8f16f4a2cb8050f2c91551faff07
evan <evan.hermier@gmail.com>
2025-06-22 22:24:50 +0200

    ajout du init dans email

---
+++ src/config/email/__init__.py
@@ -0,0 +1,9 @@
+# src/config/email/__init__.py
+"""
+Module de configuration et templates pour les emails.
+"""
+
+from .email_templates import EmailTemplates
+from .email_config import email_config, EmailConfig
+
+__all__ = ['EmailTemplates', 'email_config', 'EmailConfig']
